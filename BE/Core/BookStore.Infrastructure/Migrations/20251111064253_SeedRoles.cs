156426afdc8f24f2f67a6eed145cb14888c8527a
Trần Phạm Nhật Quân <nhatquan2k4@gmail.com>
2025-11-24 10:48:22 +0700

    fix seed role

--- BE/Core/BookStore.Infrastructure/Migrations/20251111064253_SeedRoles.cs
+++ BE/Core/BookStore.Infrastructure/Migrations/20251111064253_SeedRoles.cs
@@ -8,43 +8,5 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace BookStore.Infrastructure.Migrations
 {
     /// <inheritdoc />
-    public partial class SeedRoles : Migration
-    {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.InsertData(
-                schema: "identity",
-                table: "Roles",
-                columns: new[] { "Id", "Description", "Name" },
-                values: new object[,]
-                {
-                    { new Guid("11111111-1111-1111-1111-111111111111"), "Administrator with full system access", "Admin" },
-                    { new Guid("22222222-2222-2222-2222-222222222222"), "Regular user with basic access", "User" },
-                    { new Guid("33333333-3333-3333-3333-333333333333"), "Delivery personnel with shipping access", "Shipper" }
-                });
-        }
-
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DeleteData(
-                schema: "identity",
-                table: "Roles",
-                keyColumn: "Id",
-                keyValue: new Guid("11111111-1111-1111-1111-111111111111"));
-
-            migrationBuilder.DeleteData(
-                schema: "identity",
-                table: "Roles",
-                keyColumn: "Id",
-                keyValue: new Guid("22222222-2222-2222-2222-222222222222"));
-
-            migrationBuilder.DeleteData(
-                schema: "identity",
-                table: "Roles",
-                keyColumn: "Id",
-                keyValue: new Guid("33333333-3333-3333-3333-333333333333"));
-        }
-    }
+    // Migration seed data for roles has been removed. Please use RolePermissionSeeder.SeedAsync for seeding roles.
 }
