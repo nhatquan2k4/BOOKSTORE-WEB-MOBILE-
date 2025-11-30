194e4a4a5fe9d50dd0490762597fe1fac49c2c7a
Trần Phạm Nhật Quân <nhatquan2k4@gmail.com>
2025-12-01 00:55:42 +0700

    fix jenkinsfile

--- BE/Core/BookStore.Infrastructure/Migrations/20251111160608_SeedRolesData.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
-
-#nullable disable
-
-namespace BookStore.Infrastructure.Migrations
-{
-    /// <inheritdoc />
-    // Migration seed data for roles has been removed. Please use RolePermissionSeeder.SeedAsync for seeding roles.
-}
